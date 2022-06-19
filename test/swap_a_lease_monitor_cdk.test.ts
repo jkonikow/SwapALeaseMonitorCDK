23080a07dc078f052becdf87003d62ec7462cda0
Jason Konikow <jkon1513@gmail.com>
2022-06-18 23:23:56 -0700

    initial package setup

---
+++ test/swap_a_lease_monitor_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SwapALeaseMonitorCdk from '../lib/swap_a_lease_monitor_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/swap_a_lease_monitor_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SwapALeaseMonitorCdk.SwapALeaseMonitorCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

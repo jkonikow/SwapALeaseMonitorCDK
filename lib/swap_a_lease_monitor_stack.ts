3245c7d1f0ef2d0ab4ec002dccf96d777a97dcfa
Jkon1513 <jkon1513@gmail.com>
2022-06-21 01:00:48 -0700

    Add periodic trigger

--- lib/swap_a_lease_monitor_stack.ts
+++ lib/swap_a_lease_monitor_stack.ts
@@ -1,25 +1,31 @@
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Role, ManagedPolicy, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets'
 
 export class SwapALeaseMonitorStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const listingsBucket: s3.Bucket = this.createListingsBucket();
-    
-    const lambdaRole: iam.Role = this.createSwapALeaseMonitorLambndaRole();
-    listingsBucket.grantReadWrite(lambdaRole);
+    const listingsBucket: Bucket = this.createListingsBucket();
+    const swapALeaseMonitorRole: Role = this.createSwapALeaseMonitorLambdaRole();
+    const swapALeaseMonitorLambda = this.createSwapALeaseMonitorLambda(swapALeaseMonitorRole);
+    const periodicTrigger: Rule = this.createPeriodicTrigger();
 
-    this.createSwapALeaseMonitorLambda(lambdaRole);
+    listingsBucket.grantReadWrite(swapALeaseMonitorRole);
+    
+    periodicTrigger.addTarget(new LambdaFunction(swapALeaseMonitorLambda, {
+      retryAttempts: 0
+    }));
   }
 
-  private createSwapALeaseMonitorLambda(executionRole: iam.Role): lambda.Function {
-    return new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('lambda'),
+  private createSwapALeaseMonitorLambda(executionRole: Role): Function {
+    return new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset('lambda'),
       handler: 'hello.handler',
       functionName: "HelloFunction",
       description: "Simple hellow world function for testing event bridge connectivity",
@@ -27,22 +33,31 @@ export class SwapALeaseMonitorStack extends Stack {
     });
   }
 
-  private createSwapALeaseMonitorLambndaRole(): iam.Role {
-    return new iam.Role(this, "SwapALeaseMonitorRole", {
+  private createSwapALeaseMonitorLambdaRole(): Role {
+    return new Role(this, "SwapALeaseMonitorRole", {
       roleName: "SwapALeaseMonitorRole",
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
       managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName("AWSLambdaBasicExecutionRole"),
-        iam.ManagedPolicy.fromAwsManagedPolicyName("AWSXRayDaemonWriteAccess")
+        ManagedPolicy.fromAwsManagedPolicyName("AWSLambdaBasicExecutionRole"),
+        ManagedPolicy.fromAwsManagedPolicyName("AWSXRayDaemonWriteAccess")
       ]
     });
   }
 
-  private createListingsBucket(): s3.Bucket {
-    return new s3.Bucket(this, 'SwapALeaseMonitorListingsBucket',{
+  private createListingsBucket(): Bucket {
+    return new Bucket(this, 'SwapALeaseMonitorListingsBucket',{
       bucketName: "swap-a-lease-listings",
       removalPolicy: RemovalPolicy.DESTROY, 
       autoDeleteObjects: true
+    });
+  }
+
+  // todo make hourly after testing
+  private createPeriodicTrigger(): Rule {
+    return new Rule(this, "ScheduleRule", {
+      description: "Periodic trigger for SwapALeaseMonitor",
+      ruleName: "SwapALeaseMonitorPeriodicTrigger",
+      schedule: Schedule.cron({minute: "0", hour: "4"})
     });
   }
 }
